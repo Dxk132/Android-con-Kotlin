33dc7e63b8d36fa54af5f8540fa82e9a42a9dcc2
DxK132 <sebas.casalfa@gmail.com>
2026-08-04 10:03:55 -0600

    toast

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myfirstapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,10 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val boton = findViewById<Button>(R.id.boton)
+        boton.setOnClickListener {
+            Toast.makeText(this, "esto es un toast", Toast.LENGTH_SHORT).show()
         }
     }
 }
