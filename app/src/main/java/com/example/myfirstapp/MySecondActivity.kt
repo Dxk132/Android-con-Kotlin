5c73a4d78dbcc8c3d9a88268785760faab38d334
DxK132 <sebas.casalfa@gmail.com>
2026-08-18 10:15:25 -0600

    Recibir_datos

--- app/src/main/java/com/example/myfirstapp/MySecondActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MySecondActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myfirstapp
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,9 @@ class MySecondActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val nombre = intent.getStringExtra("nombre_usuario")
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text = nombre
     }
 }
